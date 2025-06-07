dd255cb0a000e02155cf79d6f59ddce379c237dd
mokhethi-k <mokhethikabelo@gmail.com>
2025-06-07 11:36:38 +0200

    Temperature conversion tool and exploring usage of global variable

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,22 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = (5 / 9)
+CELSIUS_TO_FAHRENHEIT_FACTOR = (9 / 5)
+
+def convert_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celsius
+
+def convert_to_fahrenheit(celsius):
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) +32
+    return fahrenheit
+
+temperature = float(input("Enter the temperature to convert: "))
+unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+
+if unit == "F":
+    result = convert_to_celsius(temperature)
+    print(f"{temperature}°F is {result}°C")
+elif unit == "C":
+    result = convert_to_fahrenheit(temperature)
+    print(f"{temperature}°C is {result}°F")
+else:
+    print("Invalid unit of measurement!")
