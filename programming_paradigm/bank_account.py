e0bc2d1cd164ad529581455c1f65bebe0c80fd11
mokhethi-k <mokhethikabelo@gmail.com>
2025-06-13 19:41:27 +0200

    coreect the the output for deposit method

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,7 +1,7 @@
 #Bank Acount Class defination
 class BankAccount:
-    def __init__(self):
-        self.account_balance = 0
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
 
     def deposit(self, amount):
         self.account_balance += amount
