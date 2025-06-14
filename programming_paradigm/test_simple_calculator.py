f172c6d007643d36bbe7617ac7fe1d57ad7485eb
mokhethi-k <mokhethikabelo@gmail.com>
2025-06-14 18:08:11 +0200

    Writing a unittest for a simple calculator Class

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,32 @@
+import unittest
+from simple_calculator import SimpleCalculator 
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_multiplyition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
+        self.assertEqual(self.calc.add(21, 0), 21)
+
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(3, 2), 1)
+        self.assertEqual(self.calc.subtract(-2, 1), -3)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(21, 0), 21)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
+        self.assertEqual(self.calc.multiply(21, 0), 0)
+
+    def test_division(self):
+        self.assertEqual(self.calc.divide(3, 2), 1.5)
+        self.assertEqual(self.calc.divide(-2, 2), -1)
+        self.assertEqual(self.calc.divide(-4, -2), 2)
+        self.assertEqual(self.calc.divide(10, 0), None)
